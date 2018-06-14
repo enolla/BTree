0ca065f9ffdb3cc1de58eb32a175c92f83b8512f
Ethan Knoll <erk5029@gmail.com>
2018-06-13 23:08:01 -0700

    Update BTree.cpp

--- BTree/BTree.cpp
+++ BTree/BTree.cpp
@@ -1,7 +1,15 @@
 #include "BTree.h"
+#include <queue>
 
 #define MAX(x,y) ((x > y) ? x : y)
 
+
+
+template<class T>
+typename BTree<T>::node* BTree<T>::getRoot()
+{
+    return root_;
+}
 
 template<class T>
 int BTree<T>::getDepth()
@@ -64,7 +72,12 @@ void BTree<T>::recInsert(node* insertNode, node* currentNode)
 {
     insertNode->parent = currentNode;
 
-    if (insertNode->data < currentNode->data)
+    
+    if (insertNode->data == currentNode->data)
+    {
+        delete insertNode;
+    }
+    else if (insertNode->data < currentNode->data)
     {
         if (!currentNode->left)
         {
@@ -95,6 +108,71 @@ void BTree<T>::clearTree()
     recClearTree(root_);
 }
 
+//good god
+//https://isocpp.org/wiki/faq/templates#dependent-name-lookup-types
+
+template<class T>
+typename BTree<T>::node* BTree<T>::LCA(T data_A, T data_B)
+{
+    std::queue<typename BTree<T>::node*> nodeQueue_A;
+    std::queue<typename BTree<T>::node*> nodeQueue_B;
+
+    dataPath(nodeQueue_A, data_A, root_);
+    dataPath(nodeQueue_B, data_B, root_);
+
+    typename BTree<T>::node* prevNodeptr;
+    typename BTree<T>::node* currentNodeAptr;
+    typename BTree<T>::node* currentNodeBptr;
+
+    prevNodeptr = root_;
+
+    while (!nodeQueue_A.empty() || !nodeQueue_B.empty())
+    {
+        if (nodeQueue_A.empty())
+        {
+            currentNodeAptr = nullptr;
+        }
+        else
+        {
+            currentNodeAptr = nodeQueue_A.front();
+        }
+
+        if (nodeQueue_B.empty())
+        {
+            currentNodeBptr = nullptr;
+        }
+        else
+        {
+            currentNodeBptr = nodeQueue_B.front();
+        }
+
+        if (currentNodeAptr != currentNodeBptr) return prevNodeptr;
+
+        nodeQueue_A.pop();
+        nodeQueue_B.pop();
+        prevNodeptr = currentNodeAptr;
+        
+    }
+
+    return nullptr;
+}
+
+template<class T>
+void BTree<T>::dataPath(std::queue<typename BTree<T>::node*>& nodeQueue, T data, node* currentNode)
+{
+    if (!currentNode) return;
+    
+    nodeQueue.push(currentNode);
+    
+    if (currentNode->data == data)
+    {
+        return;
+    }
+
+    currentNode->data > data ? dataPath(nodeQueue, data, currentNode->left) : dataPath(nodeQueue, data, currentNode->right);
+
+
+}
 
 template<class T>
 void BTree<T>::recClearTree(node* currentNode)
@@ -122,6 +200,7 @@ void BTree<T>::printTree()
 }
 
 
+
 template<class T>
 void BTree<T>::recPrint(node* currentNode)
 {
@@ -137,7 +216,5 @@ void BTree<T>::recPrint(node* currentNode)
     {
         recPrint(currentNode->right);
     }
-    
+
 }
-
-
