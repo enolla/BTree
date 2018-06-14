705e43d414522e649da0c6d2d7d4a4f930e36bd8
Ethan Knoll <erk5029@gmail.com>
2018-06-13 23:08:31 -0700

    Update BTree.h

--- BTree/BTree.h
+++ BTree/BTree.h
@@ -1,27 +1,32 @@
 #pragma once
 #include <cstddef>
+#include <queue>
 
 
 template <class T>
 class BTree
 {
+public:
     class node
     {
     public:
+        node();
         node(T init) : data(init), parent(NULL), left(NULL), right(NULL) {}
 
         T data;
         node* parent;
         node* left;
         node* right;
+        void operator=(node* rhs){this = rhs}
+        bool operator!=(node* rhs){lhs == rhs? return false: return true}
     };
 
 
-public:
     BTree() : root_(NULL) {}
     ~BTree() { clearTree(); }
 
-    node* getRoot() { return root_;}
+    node* getRoot();
+    node* LCA(T data_A, T data_B);
     int getDepth();
     int recDepth(node * currentNode);
     void insert(T data);
@@ -33,6 +38,7 @@ private:
     node* root_;
 
 
+    void dataPath(std::queue<node*>& nodeStack, T data, node * currentNode);
     void recInsert(node * insertNode, node* currentNode);
     void recClearTree(node * currentNode);
     void recPrint(node * currentNode);
